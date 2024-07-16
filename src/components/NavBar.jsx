eedd07ab33b0ea050ae4689c2e7ea3cc661b0697
3MaheshKadam <maheshkadam9298@gmail.com>
2024-07-16 17:45:49 +0530

    navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import NavBarItem from "./NavBarItem"
+
+const NavBar = () => {
+  return (
+    <div className="  p-3 dark:bg-gray-600 bg-amber-300 my-3  ">
+      <div className=" flex justify-around mx-auto max-w-6xl lg:text-lg">
+        <NavBarItem title="Top Trending" params="fetchTrending" />
+        <NavBarItem title="Most Popular" params="fetchPopular" />
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/NavBarItem.jsx
@@ -0,0 +1,26 @@
+"use client"
+import { useSearchParams } from "next/navigation"
+import Link from "next/link"
+import React from "react"
+
+const NavBarItem = ({ title, params }) => {
+  const searchParams = useSearchParams()
+  const genre = searchParams.get("genre")
+  return (
+    <div className="">
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === params
+            ? "underline underline-offset-8 decoration-4 decoration-amber-700 rounded-lg"
+            : ""
+        }`}
+        href={`
+        /?genre=${params}`}
+      >
+        <p>{title}</p>
+      </Link>
+    </div>
+  )
+}
+
+export default NavBarItem
