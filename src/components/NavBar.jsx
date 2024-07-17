3dd1c66f5d0c322430108b241dec7a6c51fd2caa
3MaheshKadam <maheshkadam9298@gmail.com>
2024-07-17 16:02:00 +0530

    card component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ const NavBar = () => {
   return (
     <div className="  p-3 dark:bg-gray-600 bg-amber-300 my-3  ">
       <div className=" flex justify-around mx-auto max-w-6xl lg:text-lg">
-        <NavBarItem title="Top Trending" params="fetchTrending" />
-        <NavBarItem title="Most Popular" params="fetchPopular" />
+        <NavBarItem title="Top Trending" params="fetchPopular" />
+        <NavBarItem title="Most Popular" params="fetchTopRated" />
       </div>
     </div>
   )
