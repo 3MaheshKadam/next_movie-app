4b747485dc89d21cb7e47e782e7de97782c2debd
3MaheshKadam <maheshkadam9298@gmail.com>
2024-07-18 19:42:04 +0530

    add loading suspense in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,16 @@
-import React from "react"
+"use client"
+
+import React, { Suspense } from "react"
 import NavBarItem from "./NavBarItem"
 
 const NavBar = () => {
   return (
-    <div className="  p-3 dark:bg-gray-600 bg-amber-300 my-3  ">
-      <div className=" flex justify-around mx-auto max-w-6xl lg:text-lg">
-        <NavBarItem title="Top Trending" params="fetchPopular" />
-        <NavBarItem title="Most Popular" params="fetchTopRated" />
+    <div className="p-3 dark:bg-gray-600 bg-amber-300 my-3">
+      <div className="flex justify-around mx-auto max-w-6xl lg:text-lg">
+        <Suspense fallback={<div>Loading...</div>}>
+          <NavBarItem title="Top Trending" params="fetchPopular" />
+          <NavBarItem title="Most Popular" params="fetchTopRated" />
+        </Suspense>
       </div>
     </div>
   )
--- src/components/NavBarItem.jsx
+++ src/components/NavBarItem.jsx
@@ -7,15 +7,14 @@ const NavBarItem = ({ title, params }) => {
   const searchParams = useSearchParams()
   const genre = searchParams.get("genre")
   return (
-    <div className="">
+    <div>
       <Link
         className={`hover:text-amber-600 font-semibold ${
           genre === params
             ? "underline underline-offset-8 decoration-4 decoration-amber-700 rounded-lg"
             : ""
         }`}
-        href={`
-        /?genre=${params}`}
+        href={`/?genre=${params}`}
       >
         <p>{title}</p>
       </Link>
